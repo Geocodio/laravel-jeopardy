909ab5333ac3e0c3a2b5a0b5ffd039e9e64a3fce
Mathias Hansen <me@codemonkey.io>
2025-08-19 21:07:25 +0200

    fix: DD value not reflected on clue board

--- resources/views/livewire/clue-display.blade.php
+++ resources/views/livewire/clue-display.blade.php
@@ -91,7 +91,12 @@
                             {{ $clue->category->name }}
                         </h2>
                         <div class="text-4xl md:text-5xl font-black text-white mt-2">
-                            ${{ number_format($clue->value) }}
+                            @if($isDailyDouble && $wagerAmount > 0)
+                                ${{ number_format($wagerAmount) }}
+                                <span class="text-2xl md:text-3xl text-yellow-400 block mt-1">(Daily Double)</span>
+                            @else
+                                ${{ number_format($clue->value) }}
+                            @endif
                         </div>
                     </div>
 
