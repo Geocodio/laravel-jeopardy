67c2c3bbfb0361a40dd99b3d7e9ce4aab04ae066
Mathias Hansen <me@codemonkey.io>
2025-08-20 17:47:40 +0200

    feat(ui): replace start button text with play icon
    Replace the "START" text on the volunteer picker button with a Heroicons play icon for improved visual appeal and intuitive user experience

--- resources/views/livewire/volunteer-picker.blade.php
+++ resources/views/livewire/volunteer-picker.blade.php
@@ -70,7 +70,9 @@
                 <div class="text-center">
                     <button wire:click="startSelection"
                             class="cursor-pointer px-16 py-8 bg-yellow-400 text-black text-4xl font-bold rounded-lg hover:bg-yellow-300 transform hover:scale-105 transition-all duration-300 shadow-2xl">
-                        START
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-16 h-16">
+                            <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
+                        </svg>
                     </button>
                 </div>
             @elseif(!$selectionComplete)
